d8cb778486567d9ad5e2ee80b51eeccb59091b97
Asritha Bodepudi <asritha.bodepudi@ukg.com>
2024-02-27 09:36:00 -0500

    more shit

--- tf-infrastructure/provider.tf
+++ tf-infrastructure/provider.tf
@@ -1,17 +1,26 @@
+# terraform {
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "~> 4.19.0"
+#    }
+#    tls = { 
+#     source = "hashicorp/tls"
+#     version = "~> 4.0.5"
+#    }
+#     random = {
+#       source  = "hashicorp/random"
+#       version = "~> 3.0"
+#     }
+#  }
+# }
+
+
 terraform {
  required_providers {
-   aws = {
-     source  = "hashicorp/aws"
-     version = "~> 4.19.0"
-   }
-   tls = { 
-    source = "hashicorp/tls"
-    version = "~> 4.0.5"
-   }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
+   aws = "hashicorp/aws"
+   tls = "hashicorp/tls"
+   random =  "hashicorp/random"
  }
 }
 
