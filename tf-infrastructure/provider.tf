c479c1b6087e2f7370959ff5d9c9c794ec83bd5d
Asritha Bodepudi <asritha.bodepudi@ukg.com>
2024-02-27 09:39:18 -0500

    testing random

--- tf-infrastructure/provider.tf
+++ tf-infrastructure/provider.tf
@@ -2,15 +2,15 @@ terraform {
  required_providers {
    aws = {
      source  = "hashicorp/aws"
-     version = "~> 4.19.0"
+     version = "4.19.0"
    }
    tls = { 
     source = "hashicorp/tls"
-    version = "~> 4.0.5"
+    version = "4.0.5"
    }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0"
+      version = "3.0"
     }
  }
  required_version = ""
